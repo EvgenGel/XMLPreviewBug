e6ab207a0f79f76f7867088e919086592308e544
Evgeniy Shmoylov <shmoilov.ee@avgd.pro>
2023-04-08 00:18:28 +0400

    init

---
+++ app/src/main/java/ru/evgengel/app/MainActivity.kt
@@ -0,0 +1,24 @@
+package ru.evgengel.app
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+class CustomView : View {
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+}
